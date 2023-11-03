90c5a696099f01ce7139d115008a63ab34eba415
Samuel Jackson Gido <samuelgido@outlook.com>
2023-11-03 14:08:12 -0500

    3rd page layout complete, working on reading combobox options from txt file

--- EFH-2/RDDataPage.xaml.cs
+++ EFH-2/RDDataPage.xaml.cs
@@ -26,6 +26,38 @@ namespace EFH_2
         public RDDataPage()
         {
             this.InitializeComponent();
+
+            List<string> duhNames = new();
+
+            try
+            {
+                using (StreamReader input = new("C:\\ProgramData\\USDA\\Shared Engineering Data\\EFH2\\duh.txt"))
+                {
+                    string? line = input.ReadLine();
+                    while (!line.Equals(""))
+                    {
+                        char[] seperator = { ' ', '\n', '\r' };
+                        string[] words = line.Split(seperator);
+
+                        duhNames.Add(words[0]);
+
+                        ComboBoxItem curr = new();
+                        curr.Content = "words[0]";
+                        uxDUH.Items.Add(curr);
+
+                        line = input.ReadLine();
+                    }
+
+                }
+            }
+            catch (Exception err)
+            {
+            }
+        }
+
+        private void PlotHydrographs(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
