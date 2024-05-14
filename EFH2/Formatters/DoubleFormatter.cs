ce2e0559dd2338bf9cd2030fcdd48a582b5583b8
Samuel Gido <samuelgido@outlook.com>
2024-05-14 11:26:47 -0500

    added double formatter

---
+++ EFH2/Formatters/DoubleFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Globalization.NumberFormatting;
+
+namespace EFH2
+{
+	public class DoubleFormatter : INumberFormatter2, INumberParser
+	{
+		public virtual double? ParseDouble(string text) => double.TryParse(text, out var dbl) ? dbl : null;
+		public virtual string Format { get; set; } = "{0:F2}"; 
+		public virtual string FormatDouble(double value) => string.Format(Format, value);
+
+		// Only need to do doubles 
+		public string FormatInt(long value) => throw new NotSupportedException();
+		public string FormatUInt(ulong value) => throw new NotSupportedException();
+		public long? ParseInt(string text) => throw new NotSupportedException();
+		public ulong? ParseUInt(string text) => throw new NotSupportedException();
+	}
+}
